348f33cf1a98ee373a2e5b880c97648076ea5a23
je jeongmin <jjmyann@hanmail.net>
2025-02-16 19:34:13 +0900

    unreal build setting 5.1 -> 5.5

--- Unreal5Project/MMO/S1/Source/S1Editor.Target.cs
+++ Unreal5Project/MMO/S1/Source/S1Editor.Target.cs
@@ -8,8 +8,8 @@ public class S1EditorTarget : TargetRules
 	public S1EditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.Add("S1");
 	}
 }
