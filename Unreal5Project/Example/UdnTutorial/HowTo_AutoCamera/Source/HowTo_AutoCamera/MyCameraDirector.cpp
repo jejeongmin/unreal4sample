23f34cae1e69791f14336f35b68cd4c07c151e1a
je jeongmin <jjmyann@hanmail.net>
2025-04-15 19:04:45 +0900

    TimeofDay ue4 -> ue5 BandoHouse 페기 udemy 예제코드는 ue5 폴더로 변경, 포팅은 이후에 진행

---
+++ Unreal5Project/Example/UdnTutorial/HowTo_AutoCamera/Source/HowTo_AutoCamera/MyCameraDirector.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyCameraDirector.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AMyCameraDirector::AMyCameraDirector()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyCameraDirector::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyCameraDirector::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	TimeToNextCameraChange -= DeltaTime;
+	if (TimeToNextCameraChange <= 0.0f)
+	{
+		// 로컬 플레이어의 컨트롤을 처리하는 액터를 찾습니다.
+		APlayerController* OurPlayerController = UGameplayStatics::GetPlayerController(this, 0);
+		if (OurPlayerController)
+		{
+			if ((OurPlayerController->GetViewTarget() != Cameras[0].Camera) && (Cameras[0].Camera != nullptr))
+				selectedCameraIndex = 0;
+			else if ((OurPlayerController->GetViewTarget() != Cameras[1].Camera) && (Cameras[1].Camera != nullptr))
+				selectedCameraIndex = 1;
+
+			TimeToNextCameraChange += Cameras[selectedCameraIndex].TimeBetweenCameraChanges;
+
+			if (Cameras[selectedCameraIndex].SmoothBlend)
+				OurPlayerController->SetViewTargetWithBlend(Cameras[selectedCameraIndex].Camera, Cameras[selectedCameraIndex].SmoothBlendTime);
+			else
+				OurPlayerController->SetViewTarget(Cameras[selectedCameraIndex].Camera);
+		}
+	}
+}
+
